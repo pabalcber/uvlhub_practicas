4814174e41690a6b69247b22e7e058789bb20b9a
Pablo Alcántara Bernal <pabloalcantarabernal@gmail.com>
2024-10-22 17:18:41 +0200

    fix: test_unit de notepad

--- app/modules/notepad/tests/test_unit.py
+++ app/modules/notepad/tests/test_unit.py
@@ -22,16 +22,3 @@ def test_sample_assertion(test_client):
     """
     greeting = "Hello, World!"
     assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
-
-def test_list_empty_notepad_get(test_client):
-    """
-    Tests access to the empty notepad list via GET request.
-    """
-    login_response = login(test_client, "user@example.com", "test1234")
-    assert login_response.status_code == 200, "Login was unsuccessful."
-
-    response = test_client.get("/notepad")
-    assert response.status_code == 200, "The notepad page could not be accessed."
-    assert b"You have no notepads." in response.data, "The expected content is not present on the page"
-
-    logout(test_client)
